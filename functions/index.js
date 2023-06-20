9db3d432b025332c9df0fe08a3f21d38c4c5ed65
titusefferian <kiritosato@gmail.com>
2023-06-20 06:07:09 +0000

    first commit

---
+++ functions/index.js
@@ -0,0 +1,56 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const {onRequest} = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
+const {Client, Events, GatewayIntentBits, EmbedBuilder, bold} = require("discord.js");
+const dayjs = require("dayjs");
+const {token} = require("./config.json");
+const objectSupport = require("dayjs/plugin/objectSupport");
+dayjs.extend(objectSupport);
+
+// const startDate = dayjs("2023-06-20T13:00:00+07:00");
+const endDate = dayjs("2023-06-20T13:00:00+07:00");
+const isFirstDay = true;
+const diff = endDate.diff(dayjs(), "day");
+const title = `${bold("STEP by STEP!")}`;
+const imageUrl = "https://pbs.twimg.com/media/Fy9xvwiaYAAFzYp?format=jpg&name=900x900";
+
+const description = () => {
+  if (isFirstDay) {
+    return `Event ${title} telah dimulai`;
+  }
+  if (diff === 0) {
+    return `Event telah dimulai dan akan ${bold("berakhir hari ini.")}`;
+  }
+  if (diff < 0) {
+    return `Event telah berakhir.`;
+  }
+  return `Event telah dimulai dan akan berakhir dalam ${bold(`${diff} hari lagi.`)}`;
+};
+
+const eventsReminderJP = onRequest({region: "asia-southeast2", maxInstances: 1}, (_, response) => {
+  // logger.info("Hello logs!", { structuredData: true });
+  const client = new Client({intents: [GatewayIntentBits.Guilds]});
+  client.once(Events.ClientReady, (c) => {
+    console.log(`Ready! Logged in as ${c.user.tag}`);
+    const embedBuilder = new EmbedBuilder()
+        .setImage(imageUrl)
+        .setTitle(title)
+        .setDescription(description());
+    client.channels.cache.get("912576389130125396").send({embeds: [embedBuilder]}).then(() => {
+      response.status(200).send("200");
+    });
+  });
+
+  // Log in to Discord with your client's token
+  client.login(token);
+});
+
+module.exports = {eventsReminderJP};
