acde9825638fc58e9de457b4c4ce5e5218d80464
titusefferian <kiritosato@gmail.com>
2023-07-01 02:11:51 +0000

    jp update

--- functions/index.js
+++ functions/index.js
@@ -17,12 +17,13 @@ const {token} = require("./config.json");
 const objectSupport = require("dayjs/plugin/objectSupport");
 dayjs.extend(objectSupport);
 
-const JP_CHANNEL = "1119110367897985116";
-const endDate = dayjs("2023-06-28T19:00:00+07:00");
+const {JP_CHANNEL} = require("./constant");
+
+const endDate = dayjs("2023-07-09T19:00:00+07:00");
 const isFirstDay = false;
 const diff = endDate.diff(dayjs(), "day");
-const title = `${bold("STEP by STEP!")}`;
-const imageUrl = "https://pbs.twimg.com/media/Fy9xvwiaYAAFzYp?format=jpg&name=900x900";
+const title = `${bold("一期一会な百鬼夜行！？")}`;
+const imageUrl = "https://pbs.twimg.com/media/FzxNlSTacAAHknW?format=jpg&name=900x900";
 
 const description = () => {
   if (isFirstDay) {
